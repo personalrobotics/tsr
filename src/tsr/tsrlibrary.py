aae9ec4e70608e432d2600c932994418382c04dc
Shushman Choudhury <shushman@ash.personalrobotics.ri.cmu.edu>
2017-04-06 10:30:01 -0400

    Added absolute_import to tsrlibrary.py

--- src/tsr/tsrlibrary.py
+++ src/tsr/tsrlibrary.py
@@ -29,6 +29,7 @@
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 import collections, functools, logging, numpy, os.path
 
 logger = logging.getLogger(__name__)
